5e048f1c55a3c9ddbff3f8fb3dcb850439b2b9cb
madan5897 <madanhs5897@gmail.com>
2023-01-07 23:25:32 +0530

    initial commit

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map((ele) => {
+    if (ele.profession === "developer") {
+      console.log(ele.name);
+    }
+  });
 }
+PrintDeveloperswithMap();
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach((ele) => {
+    if (ele.profession === "developer") {
+      console.log(ele.name);
+    }
+  });
 }
+PrintDeveloperbyForEach();
 
+const newElement = { id: 4, name: "susan", age: "20", profession: "intern" };
 function addData() {
   //Write your code here
+  arr.push(newElement);
+  console.log(arr);
 }
+addData();
 
 function removeAdmin() {
   //Write your code here
+  const newArr = arr.filter((ele) => {
+    return ele.profession !== "admin";
+  });
+  console.log(newArr);
 }
+removeAdmin();
 
 function concatenateArray() {
   //Write your code here
+  let arr1 = [
+    { id: 4, name: "joe", age: "25", profession: "Detective" },
+    { id: 5, name: "joi", age: "22", profession: "chef" },
+    { id: 6, name: "Dark", age: "26", profession: "Gamer" },
+  ];
+  const combinedArr = arr.concat(arr1);
+  console.log(combinerArr);
 }
+concatenateArray();
